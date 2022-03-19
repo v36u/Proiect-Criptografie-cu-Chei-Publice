fa642742d422e0d6d74952effe56e3c6903b1a12
Vlad Dumitru <dumitru.vlad0213@gmail.com>
2022-03-19 13:31:07 +0200

    Generare număr aleatoriu prim

--- EntitateRSA.h
+++ EntitateRSA.h
@@ -1,18 +1,37 @@
 #pragma once
 
 #include <string>
-
 using std::string;
 
+#include <unordered_set>
+using std::unordered_set;
+
+#include <random>
+using std::random_device;
+using std::mt19937;
+
+// ReSharper disable once CppInconsistentNaming
 class EntitateRSA
 {
+
+    static unordered_set<size_t> _chei_private; // Pentru a evita coliziuni
+
+    static random_device _generator_seed; // https://www.cplusplus.com/reference/random/random_device/
+    static mt19937 _generator_numere_aleatorii; // https://www.cplusplus.com/reference/random/mt19937/
+
     const string _nedefinit = "NEDEFINIT";
 
     size_t _cheie_privata;
     size_t _cheie_publica;
     string _nume;
 
-    static size_t CelMaiMareDivizorComun(const int&, const int&);
+    static size_t CelMaiMareDivizorComun(const size_t&, const size_t&);
+
+    static size_t GenerareNumarRandom();
+
+    static bool EstePrim(const size_t&);
+
+    static size_t GenerareNumarPrimRandom();
 
 public:
     EntitateRSA();
--- Proiect-Criptografie-cu-Chei-Publice.cpp
+++ Proiect-Criptografie-cu-Chei-Publice.cpp
@@ -1,42 +1,8 @@
-#include "EntitateRSA.h"
+﻿#include "EntitateRSA.h"
 
 int main()
 {
-    double p = 13;
-    double q = 11;
-    double n = p * q;
-    
-    double phi = (p - 1) * (q - 1);
-
-    double e = 7;
-
-    while (e < phi)
-    {
-        const auto track = gcd(e, phi);
-        if (track == 1)
-            break;
-        e++;
-    }
-
-    double d1 = 1 / e;
-    double d = fmod(d1, phi);
-
-    double message = 9;
-    double c = pow(message, e); 
-    double m = pow(c, d);
-
-    c = fmod(c, n);
-    m = fmod(m, n);
-
-    cout << "Original Message = " << message;
-    cout << "\n" << "p = " << p;
-    cout << "\n" << "q = " << q;
-    cout << "\n" << "n = pq = " << n;
-    cout << "\n" << "phi = " << phi;
-    cout << "\n" << "e = " << e;
-    cout << "\n" << "d = " << d;
-    cout << "\n" << "Encrypted message = " << c;
-    cout << "\n" << "Decrypted message = " << m;
+    // Nimic de văzut aici
 
     return EXIT_SUCCESS;
 }
