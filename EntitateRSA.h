a4b5ff0df1455a0bc2248d90e85e2440776abc19
Vlad Dumitru <dumitru.vlad0213@gmail.com>
2022-03-20 18:57:49 +0200

    Creare și testare metode de encriptare și decriptare - WIP

--- EntitateRSA.h
+++ EntitateRSA.h
@@ -2,6 +2,7 @@
 
 #include <string>
 using std::string;
+using std::to_string;
 
 #include <unordered_set>
 using std::unordered_set;
@@ -12,24 +13,38 @@ using std::mt19937;
 
 #include <iostream>
 using std::cout;
+using std::cin;
+using std::ostream;
 
 #include <cmath>
 using std::fmod;
+using std::pow;
+
+#include <stdexcept>
+using std::logic_error;
 
 // ReSharper disable once CppInconsistentNaming
 class EntitateRSA
 {
+    static const string NEDEFINIT;
+    static const size_t LIMITA_NUMAR_ALEATOR;
+
     // Pentru a evita coliziuni - șansă extrem de mică, dar de dragul conceptului
     static unordered_set<long double> _chei_private;
 
     static random_device _generator_seed; // https://www.cplusplus.com/reference/random/random_device/
     static mt19937 _generator_numere_aleatorii; // https://www.cplusplus.com/reference/random/mt19937/
 
-    const string _nedefinit = "NEDEFINIT";
-
     long double _cheie_privata;
     long double _cheie_publica;
+    size_t _produs_numere_prime;
     string _nume;
+
+    enum TipCheie
+    {
+        Privata,
+        Publica
+    };
 
     static size_t CelMaiMareDivizorComun(const size_t&, const size_t&);
 
@@ -41,10 +56,24 @@ class EntitateRSA
 
     static size_t GenerareCheiePrivata(const size_t&);
 
+    long double GetCheieFolosita(const TipCheie&) const;
+
+    long double Encriptare(const long double& p_mesaj, const TipCheie&) const;
+
+    long double Decriptare(const long double& p_mesaj, const TipCheie&) const;
+                                                                  
 public:
     EntitateRSA();
     explicit EntitateRSA(const string&);
     ~EntitateRSA();
 
     void GenerareChei();
+
+    long double EncriptareCuCheiePrivata(const long double&) const;
+    long double EncriptareCuCheiePublica(const long double&) const;
+
+    long double DecriptareCuCheiePrivata(const long double&) const;
+    long double DecriptareCuCheiePublica(const long double&) const;
+
+    friend ostream& operator<<(ostream& p_stream, const EntitateRSA& p_entitate);
 };
--- Proiect-Criptografie-cu-Chei-Publice.cpp
+++ Proiect-Criptografie-cu-Chei-Publice.cpp
@@ -5,6 +5,45 @@
 
 #pragma execution_character_set("utf-8")
 
+void AfisareStareCurentaMesaj(const long double& p_mesaj)
+{
+    cout << "\nAcum mesajul arată așa: " << p_mesaj;
+}
+
+void TrimitereMesajDeLaOEntitateLaAltaEntitate(EntitateRSA& p_emitator, EntitateRSA& p_receptor)
+{
+    long double mesaj;
+    cout << "\nIntroduceți mesajul care trebuie trimis de la " << p_emitator << " către " << p_receptor << " (număr real): ";
+    cin >> mesaj;
+
+    mesaj = p_emitator.EncriptareCuCheiePrivata(mesaj);
+    cout << mesaj << '\n';
+    mesaj = p_emitator.DecriptareCuCheiePublica(mesaj);
+    cout << mesaj << '\n';
+
+    // mesaj = p_emitator.EncriptareCuCheiePrivata(mesaj);
+    // cout << '\n' << p_emitator << " encriptează mesajul cu cheia sa privată pentru a se putea dovedi că mesajul ar fi putut să fie scris doar de el.";
+    // AfisareStareCurentaMesaj(mesaj);
+    //
+    // cout << '\n';
+    //
+    // mesaj = p_receptor.EncriptareCuCheiePublica(mesaj);
+    // cout << "\nApoi, " << p_emitator << " encriptează mesajul cu cheia publică a lui " << p_receptor << " pentru a se asigura că doar " << p_receptor << " poate decripta mesajul.";
+    // AfisareStareCurentaMesaj(mesaj);
+    //
+    // cout << '\n';
+    //
+    // mesaj = p_receptor.DecriptareCuCheiePrivata(mesaj);
+    // cout << '\n' << p_receptor << " decriptează mesajul cu cheia sa privată. Astfel se confirmă faptul că mesajul este menit să ajungă la " << p_receptor << '.';
+    // AfisareStareCurentaMesaj(mesaj);
+    //
+    // cout << '\n';
+    //
+    // mesaj = p_receptor.DecriptareCuCheiePublica(mesaj);
+    // cout << "\nApoi, " << p_receptor << " decriptează mesajul cu cheia publică a lui " << p_emitator << ". Astfel se confirmă faptul că mesajul a putut fi scris doar de " << p_emitator << '.';
+    // AfisareStareCurentaMesaj(mesaj);
+}
+
 int main()
 {
     SetConsoleOutputCP(65001);
@@ -17,7 +56,17 @@ int main()
     auto alice = new EntitateRSA("Alice");
     alice->GenerareChei();
 
+    cout << '\n';
+
+    TrimitereMesajDeLaOEntitateLaAltaEntitate(*bob, *alice);
+
     delete bob;
     delete alice;
+
+    // Ca să fie ceva mai clean la finalul execuției
+    cout << '\n';
+    cin.ignore();
+    cin.get();
+
     return EXIT_SUCCESS;
 }
